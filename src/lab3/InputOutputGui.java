f21a639d34822fff6069ffb4613670d51b3ea763
mgilbertson <mgilbertson@my.wctc.edu>
2014-11-03 17:54:07 -0600

    Initial Commit

--- src/lab3/InputOutputGui.java
+++ src/lab3/InputOutputGui.java
@@ -4,11 +4,12 @@ import javax.swing.JOptionPane;
 
 /**
  * This class is responsible for all input and output in the program.
- * 
- * @author  Jim Lombardo, jlombardo@wctc.edu
+ *
+ * @author Jim Lombardo, jlombardo@wctc.edu
  * @version 1.00
  */
 public class InputOutputGui {
+
     private NameService nameService;
 
     public InputOutputGui() {
@@ -16,12 +17,21 @@ public class InputOutputGui {
     }
 
     public void startConversation() {
-        
-        String fullName = JOptionPane.showInputDialog("Enter full name:");
-        String lastName = nameService.extractLastName(fullName);
-        String msg = "Your last name is: " + lastName;
-        JOptionPane.showMessageDialog(null, msg);
-        
+        boolean pass = false;
+        while (pass == false) {
+            String fullName = JOptionPane.showInputDialog("Enter full name:");
+            String lastName = "";
+            try {
+                lastName = nameService.extractLastName(fullName);
+                pass = true;
+                String msg = "Your last name is: " + lastName;
+                JOptionPane.showMessageDialog(null, msg);
+            } catch (Exception e) {
+                JOptionPane.showMessageDialog(null, "Error: " + e.getMessage() + "\nPlease try again");
+            }
+
+        }
+
     }
-     
+
 }
